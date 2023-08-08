32a29f8e5766019c04c817f556d7e20cfe4afc51
santosh <santoshpisekumar@gmail.com>
2023-08-08 13:20:43 +0530

    initial commit

---
+++ src/gitdemoproject/Git.java
@@ -0,0 +1,19 @@
+package gitdemoproject;
+
+public class Git {
+	
+
+	public static void main(String[] args) {
+		
+		//print the hello world
+		System.out.println("HELLO WORLD");
+		
+		//print sum of two numbers
+		
+		int a=10,b=20;
+		System.out.println(" THE  SUME OF TWO VALUE IS :  " +(a+b));
+		
+	}	
+
+
+}
